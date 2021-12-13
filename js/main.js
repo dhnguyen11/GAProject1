89da3f8fb72de43454cbd698114790b4d92fa8c9
dhnguyen01 <davidhuongnguyen@gmail.com>
2021-12-13 10:43:54 -0800

    Adding static variables.  Card object commented out.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
+// This array holds the values of the cards.
+// Note that Ace is an array of 2 values. 
+// Aces are worth either 1 or 11, so the value must be selected at runtime.
 const values = [[1, 11], 2, 3, 4, 5, 6, 7, 8, 9, 10 ,10 ,10 ,10];
-class Card {
-    constructor(face, suit, faceup) {
-        this.face = face;
-        this.suit = suit;
-        this.faceup = faceup;
-    }
-    turnFaceup() {
-        this.faceup = true;
-    }
-}
+
+// This array holds the possible face values of the cards.
+// The array is set up to be able to identify the cards as classes.
+// This allows for ease of use with cardstarter.css.
+// It is ordered the same as the values array.
+const faces = ['A', 'r02', 'r03', 'r04', 'r05', 'r06', 'r07', 'r08', 'r09', 'r10', 'J', 'Q', 'K'];
+
+// This array holds the possible suits that a card can be.
+const suits = ["spades", "hearts", "diamonds", "clubs"];
+
+
+
+// Is this actually needed?  Might be a simpler way.
+// class Card {
+//     constructor(face, suit, faceup) {
+//         this.face = face;
+//         this.suit = suit;
+//         this.faceup = faceup;
+//     }
+//     turnFaceup() {
+//         this.faceup = true;
+//     }
+// }
+
