3a4afd6ea5664d6e5335cffd622671ae347dd938
dhnguyen01 <davidhuongnguyen@gmail.com>
2021-12-13 10:28:03 -0800

    Adding card class and array of card values

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-const cardValues = [];
+const values = [[1, 11], 2, 3, 4, 5, 6, 7, 8, 9, 10 ,10 ,10 ,10];
 class Card {
-    constructor() {
-
+    constructor(face, suit, faceup) {
+        this.face = face;
+        this.suit = suit;
+        this.faceup = faceup;
     }
     turnFaceup() {
         this.faceup = true;
