6f00472063eebcad6b554fff8c8f27ffcaf385d2
dhnguyen01 <davidhuongnguyen@gmail.com>
2021-12-13 11:00:39 -0800

    Checking if added cards appear in game, minor updates to card object

--- js/main.js
+++ js/main.js
@@ -10,19 +10,25 @@ const values = [[1, 11], 2, 3, 4, 5, 6, 7, 8, 9, 10 ,10 ,10 ,10];
 const faces = ['A', 'r02', 'r03', 'r04', 'r05', 'r06', 'r07', 'r08', 'r09', 'r10', 'J', 'Q', 'K'];
 
 // This array holds the possible suits that a card can be.
-const suits = ["spades", "hearts", "diamonds", "clubs"];
+const suits = ['spades', 'hearts', 'diamonds', 'clubs'];
 
 
 
 // Is this actually needed?  Might be a simpler way.
 // class Card {
-//     constructor(face, suit, faceup) {
+//     constructor(face, suit, value, faceup) {
 //         this.face = face;
 //         this.suit = suit;
+//         this.value = value;
 //         this.faceup = faceup;
 //     }
 //     turnFaceup() {
 //         this.faceup = true;
 //     }
 // }
-
+const playerHandEl = document.getElementById('player-hand');
+for (let i = 0; i < 10; i++) {
+    const newEl = document.createElement('li');
+    newEl.classList.add(`spades`, `card`, `large`, faces[i]);
+    playerHandEl.appendChild(newEl);
+}
