e9c3ec318afc1abcd3cef45473511f04225c3f79
dhnguyen01 <davidhuongnguyen@gmail.com>
2021-12-13 14:36:51 -0800

    Render function fully operational, adding render test functions

--- js/main.js
+++ js/main.js
@@ -36,6 +36,9 @@ let computerScore;
 let playerHand;
 let computerHand;
 
+// Defining the used suits and values
+let usedCards;
+
 // Caching variables
 // These caches will be used to run the render function, as well as other functions
 const playerHandEl = document.getElementById('player-hand');
@@ -51,9 +54,69 @@ function init() {
     computerScore = 0;
     playerHand = [];
     computerHand = [];
+    usedCards = [];
     render();
 }
+
+// Render function
+// Updates the DOM with all new information.
+// This will place new objects onto the GUI.
 function render() {
     console.log('render function is online');
+    playerScoreEl.innerText = `Player Score: ${playerScore}`;
+    computerScoreEl.innerText = `Dealer Score: ${computerScore}`;
+    while (playerHandEl.firstChild) {
+        playerHandEl.removeChild(playerHandEl.firstChild)
+    }
+    while (computerHandEl.firstChild) {
+        computerHandEl.removeChild(computerHandEl.firstChild)
+    }
+    playerHand.forEach( (card) => {
+        const newEl = document.createElement('div');
+        newEl.classList.add('card', 'large', card.face, card.suit);
+        playerHandEl.append(newEl);
+    });
+    computerHand.forEach( (card) => {
+        const newEl = document.createElement('div');
+        if (card.faceup === false) {
+            newEl.classList.add('back-red', 'card', 'large');
+        }
+        else {
+            newEl.classList.add('card', 'large', card.face, card.suit);
+        }
+        computerHandEl.append(newEl);
+    });
 }
 init();
+
+function renderTestOne() {
+    while (playerHand.length > 0) {
+        playerHand.pop();
+    }
+    while (computerHand.length > 0) {
+        computerHand.pop();
+    }
+    playerHand.push(new Card(faces[0], suits[0], values[0], true));
+    playerHand.push(new Card(faces[1], suits[2], values[1], true));
+    playerHand.push(new Card(faces[2], suits[3], values[2], true));
+    computerHand.push(new Card(faces[0], suits[0], values[0], true));
+    computerHand.push(new Card(faces[1], suits[2], values[1], true));
+    computerHand.push(new Card(faces[2], suits[3], values[2], true));
+    render();
+}
+
+function renderTestTwo() {
+    while (playerHand.length > 0) {
+        playerHand.pop();
+    }
+    while (computerHand.length > 0) {
+        computerHand.pop();
+    }
+    playerHand.push(new Card(faces[2], suits[1], values[2], true));
+    playerHand.push(new Card(faces[10], suits[2], values[10], true));
+    playerHand.push(new Card(faces[5], suits[0], values[5], true));
+    computerHand.push(new Card(faces[3], suits[0], values[3], true));
+    computerHand.push(new Card(faces[11], suits[2], values[11], false));
+    computerHand.push(new Card(faces[7], suits[3], values[7], true));
+    render();
+}
