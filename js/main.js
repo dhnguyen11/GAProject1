ec505d517f5966dc0f3308c5df4a8db3c67cdf16
dhnguyen01 <davidhuongnguyen@gmail.com>
2021-12-13 11:38:11 -0800

    Adjusting margins on player and computer hands to ensure no unnecessary space

--- js/main.js
+++ js/main.js
@@ -12,20 +12,21 @@ const faces = ['A', 'r02', 'r03', 'r04', 'r05', 'r06', 'r07', 'r08', 'r09', 'r10
 // This array holds the possible suits that a card can be.
 const suits = ['spades', 'hearts', 'diamonds', 'clubs'];
 
+// Constructor for the cards.
+// Allows all of the data for an individual card to be stored in one place.
+class Card {
+    constructor(face, suit, value, faceup) {
+        this.face = face;
+        this.suit = suit;
+        this.value = value;
+        this.faceup = faceup;
+    }
+    turnFaceup() {
+        this.faceup = true;
+    }
+}
 
 
-// Is this actually needed?  Might be a simpler way.
-// class Card {
-//     constructor(face, suit, value, faceup) {
-//         this.face = face;
-//         this.suit = suit;
-//         this.value = value;
-//         this.faceup = faceup;
-//     }
-//     turnFaceup() {
-//         this.faceup = true;
-//     }
-// }
 const playerHandEl = document.getElementById('player-hand');
 for (let i = 0; i < 4; i++) {
     const newEl = document.createElement('div');
